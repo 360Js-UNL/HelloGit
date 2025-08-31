a9d4e3d9558507be359b8b725fb9153751c67a91
akira <akira@192.168.0.89>
2025-08-31 14:32:41 -0500

    Added my name and major

--- Hello.java
+++ Hello.java
@@ -4,6 +4,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("I love the School of Computing.\n");
+		
+		System.out.println("Hello World, my name is Argos!");
+		System.out.println("My major is Computer Engineering!");
 
 	}
 
